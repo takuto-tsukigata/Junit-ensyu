77b57dcf72b87284d954d08b8f533454c307b22c
takuto-tsukigata <takuto.tsukigata@rakus-partners.co.jp>
2022-02-14 17:23:24 +0900

    2-1を修正

--- src/test/java/junit/tutorial/ex02/e01/FizzBuzzTest.java
+++ src/test/java/junit/tutorial/ex02/e01/FizzBuzzTest.java
@@ -1,20 +1,47 @@
 package junit.tutorial.ex02.e01;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertIterableEquals;
 
+import java.util.Arrays;
 import java.util.List;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class FizzBuzzTest extends FizzBuzz {
 
-	@Test
-	void testAssert() {
-		FizzBuzz fizzBuzz = new FizzBuzz();
-		List<String> result1 = fizzBuzz.createFizzBuzzList(16);
-		assertEquals("[1,2,Fizz,4,Buzz,Fizz,7,8,Fizz,Buzz,11,Fizz,13,14,FizzBuzz,16]", result1,
-				"fizzBuzz.createFizzBuzzListの引数が16の場合");
+	FizzBuzz fizzBuzz;
 
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+		fizzBuzz = new FizzBuzz();
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	@DisplayName("16までチェック")
+	void test() {
+		List<String> expected = Arrays.asList("1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz", "11",
+				"Fizz", "13", "14", "FizzBuzz", "16");
+		List<String> actual = fizzBuzz.createFizzBuzzList(16);
+		assertAll("createFizzBuzz", () -> assertEquals(expected, actual), () -> assertIterableEquals(expected, actual));
 	}
 
 }
