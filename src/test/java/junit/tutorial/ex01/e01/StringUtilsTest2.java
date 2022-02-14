554d85683057defad3078b8e6196344fe6d0e438
takuto-tsukigata <takuto.tsukigata@rakus-partners.co.jp>
2022-02-14 10:01:23 +0900

    first commit

---
+++ src/test/java/junit/tutorial/ex01/e01/StringUtilsTest2.java
@@ -0,0 +1,14 @@
+package junit.tutorial.ex01.e01;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class StringUtilsTest2 {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
